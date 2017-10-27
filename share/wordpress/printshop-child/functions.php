8d2019cf004a5bfdf4269ff9c2cd934e55f1e2a7
Laro DEE <larodi@gmail.com>
2017-10-27 13:15:23 +0300

    wordpress integration

--- share/wordpress/printshop-child/functions.php
+++ share/wordpress/printshop-child/functions.php
@@ -1,19 +1,22 @@
 <?php
 
 function pechatar_theme_enqueue_styles() {
+    if ( WP_DEBUG ) {
+        error_reporting( E_ALL );
+
         // as per https://codex.wordpress.org/Child_Themes
-        $parent_style = 'pechatar-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+        $parent_style = 'printshop-style-home';
     
         wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'child-style',
+        wp_enqueue_style( 'pechatar-style',
             get_stylesheet_directory_uri() . '/style.css',
             array( $parent_style ),
             wp_get_theme()->get('Version')
         );
     }
+}
 
 add_action( 'wp_enqueue_scripts', 'pechatar_theme_enqueue_styles' );
-
 
 /**
  * Opening div for our content wrapper
@@ -21,7 +24,7 @@ add_action( 'wp_enqueue_scripts', 'pechatar_theme_enqueue_styles' );
 add_action('woocommerce_before_main_content', 'pechatar_open_div', 5);
 
 function pechatar_open_div() {
-    echo '<div class="pechatar-div">';
+    echo '<div class="pechatar-div"><h3> here comes 🐻 </h3>';
 }
 
 /**
